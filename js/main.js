1c325bb967399961e26df87682d49ffc4c8f69e1
Kara Anderson <KaraA@Admins-MacBook-Pro.local>
2016-04-19 11:11:19 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function shrinkNav() {
+    window.addEventListener('scroll', function (e) {
+        var distanceY = window.pageYOffset ||   document.documentElement.scrollTop,
+            shrinkOn = 300;
+        if (distanceY > shrinkOn) {
+            document.getElementById("header").className += " smaller";
+            document.getElementById("logo").className += " smaller";
+        } else {
+            if (distanceY <= shrinkOn) {
+                document.getElementById("header").className -= " smaller";
+                document.getElementById("logo").className -= " smaller";
+            }
+        }
+    });
+}
+
+window.onload = shrinkNav();

86c934a732094bdc0c4d9c9b199b1159a9b0a698
Kara Anderson <KaraA@admins-mbp.uwcm.org>
2016-05-03 16:12:49 -0400

    Add diagonal separators

--- js/main.js
+++ js/main.js
@@ -1,19 +1,63 @@
-/* Sticky shrinking nav bar */
-
 function shrinkNav() {
-    window.addEventListener('scroll', function(e) {
-        var distanceY = window.pageYOffset ||   document.documentElement.scrollTop,
-            shrinkOn = 300;
+    window.addEventListener('scroll', function (e) {
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 300,
+            header = document.getElementById('header'),
+            logo = document.getElementById('logo'),
+            navList = document.getElementById('nav-list'),
+            navIcon = document.getElementById('nav-icon-cont');
         if (distanceY > shrinkOn) {
-            document.getElementById("header").className += " smaller";
-            document.getElementById("logo").className += " smaller";
+            header.className += ' smaller';
+            logo.className += ' smaller';
+            navList.className += ' smaller';
+            navIcon.className += ' smaller';
         } else {
             if (distanceY <= shrinkOn) {
-                document.getElementById("header").className -= " smaller";
-                document.getElementById("logo").className -= " smaller";
+                header.className -= ' smaller';
+                logo.className -= ' smaller';
+                navList.className -= ' smaller';
+                navIcon.className -= ' smaller';
             }
         }
     });
 }
 
 window.onload = shrinkNav();
+
+/* Sticky shrinking nav bar
+
+function shrinkNav() {
+    window.addEventListener('scroll', function(e) {
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 300;
+        if (distanceY > shrinkOn) {
+            document.getElementById('header').classList.toggle('smaller');
+            document.getElementById('logo').classList.toggle('smaller');
+            document.getElementById('nav-list').classList.toggle('smaller');
+
+            document.getElementById('nav-icon').classList.toggle('smaller');
+        } else {
+            if (distanceY <= shrinkOn) {
+                document.getElementById('header').classList.remove('smaller');
+                document.getElementById('logo').classList.remove('smaller');
+                document.getElementById('nav-list').classList.remove('smaller');
+
+                document.getElementById('nav-icon').classList.remove('smaller');
+            }
+        }
+    });
+}
+*/
+
+  /* Open nav menu when the hamburger icon is clicked. */
+    var menu = document.querySelector('#nav-icon');
+    var main = document.querySelector('main');
+    var drawer = document.querySelector('#nav-list');
+
+    menu.addEventListener('click', function(e) {
+      drawer.classList.toggle('open');
+      e.stopPropagation();
+    });
+    main.addEventListener('click', function() {
+      drawer.classList.remove('open');
+    });

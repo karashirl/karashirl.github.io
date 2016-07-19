5adc20ef9f7246e959a720f19f6dfa88643d0b18
Kara Anderson <karashirl@gmail.com>
2016-07-19 12:29:16 -0400

    Add header animation and image fade on scroll

--- js/main.js
+++ js/main.js
@@ -1,63 +1,72 @@
-function shrinkNav() {
-    window.addEventListener('scroll', function (e) {
-        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-            shrinkOn = 300,
-            header = document.getElementById('header'),
-            logo = document.getElementById('logo'),
-            navList = document.getElementById('nav-list'),
-            navIcon = document.getElementById('nav-icon-cont');
+var elementsToFade = [$('#bmorehoods'), $('#resume'), $('#headshot')],
+    headerElements = [$('#header'), $('#logo'), $('#nav-list'), $('#nav-icon-cont')];
+
+// Open nav menu when the hamburger icon is clicked
+var menu = $('#nav-icon'),
+main = $('main'),
+drawer = $('#nav-list');
+
+menu.on('click', function(e) {
+    drawer.toggleClass('open');
+    e.stopPropagation();
+});
+
+main.on('click', function() {
+    drawer.toggleClass('open');
+});
+
+// Shrink navigation & animate header opacity, title & subtitle on scroll
+function headerAnimate() {
+    var distanceY = $(window).scrollTop(),
+    winWidth = $(window).width(),
+    docWidth = $(document).width(),
+    shrinkOn = 300;
+
+    for (i = 0; i < headerElements.length; i++) {
         if (distanceY > shrinkOn) {
-            header.className += ' smaller';
-            logo.className += ' smaller';
-            navList.className += ' smaller';
-            navIcon.className += ' smaller';
+            headerElements[i].addClass('smaller');
         } else {
             if (distanceY <= shrinkOn) {
-                header.className -= ' smaller';
-                logo.className -= ' smaller';
-                navList.className -= ' smaller';
-                navIcon.className -= ' smaller';
+                headerElements[i].removeClass('smaller');
             }
         }
-    });
+    }
+
+    // Animate header text
+    var title = $('#title'),
+        subtitle = $('#subtitle'),
+        mainImg = $('#main-img');
+
+    title.css( { opacity : (1 / distanceY) * 200 - 0.4 } );
+    subtitle.css( { opacity : (1 / distanceY) * 200 - 0.4 } );
+    mainImg.css( { opacity : (1 / distanceY) * 200 - 0.25 } );
+
+    if (winWidth > 767) {
+        title.css( { marginRight : distanceY } );
+        title.css( { marginBottom : (distanceY / 4) } );
+        subtitle.css( { marginLeft : distanceY } );
+        subtitle.css( { marginTop : (distanceY / 4) } );
+    }
+
+    if (title.css('opacity') == 0) {
+        title.css( { 'display' : 'none' } );
+        subtitle.css( { 'display' : 'none'  } );
+    } else {
+        title.css( { 'display' : 'block' } );
+        subtitle.css( { 'display' : 'block'  } );
+    }
 }
 
-window.onload = shrinkNav();
-
-/* Sticky shrinking nav bar
-
-function shrinkNav() {
-    window.addEventListener('scroll', function(e) {
-        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-            shrinkOn = 300;
-        if (distanceY > shrinkOn) {
-            document.getElementById('header').classList.toggle('smaller');
-            document.getElementById('logo').classList.toggle('smaller');
-            document.getElementById('nav-list').classList.toggle('smaller');
-
-            document.getElementById('nav-icon').classList.toggle('smaller');
-        } else {
-            if (distanceY <= shrinkOn) {
-                document.getElementById('header').classList.remove('smaller');
-                document.getElementById('logo').classList.remove('smaller');
-                document.getElementById('nav-list').classList.remove('smaller');
-
-                document.getElementById('nav-icon').classList.remove('smaller');
-            }
+// Fade in images when visible
+function fadeIn(elem) {
+    for (i = 0; i < elementsToFade.length; i++) {
+        if (($(window).scrollTop() + ($(window).height())) > elementsToFade[i].offset().top + 225) {
+            elementsToFade[i].fadeTo(700, 1);
         }
-    });
+    }
 }
-*/
 
-  /* Open nav menu when the hamburger icon is clicked. */
-    var menu = document.querySelector('#nav-icon');
-    var main = document.querySelector('main');
-    var drawer = document.querySelector('#nav-list');
-
-    menu.addEventListener('click', function(e) {
-      drawer.classList.toggle('open');
-      e.stopPropagation();
-    });
-    main.addEventListener('click', function() {
-      drawer.classList.remove('open');
-    });
+$(window).on('scroll', function(){
+   headerAnimate();
+   fadeIn(elementsToFade);
+});

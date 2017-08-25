e447961f53907ca2edfef730ab319178911b91b8
karashirl <karashirl@gmail.com>
2017-08-25 10:23:49 -0400

    Add grid beta portfolio

--- js/main.js
+++ js/main.js
@@ -1,75 +1,118 @@
-var elementsToFade = [$('.projects').find('.project-blurb'), $('#headshot')],
-    headerElements = [$('#header'), $('#logo'), $('#nav-list'), $('#nav-icon-cont')];
+// var elementsToFade = [$('.projects').find('.project-blurb'), $('#headshot')],
+//     headerElements = [$('#header'), $('#logo'), $('#nav-list'), $('#nav-icon-cont')];
 
 // Open nav menu when the hamburger icon is clicked
-var menu = $('#nav-icon'),
-main = $('main'),
-drawer = $('#nav-list');
+// var menu = $('#nav-icon'),
+// main = $('main'),
+// drawer = $('#nav-list');
 
-menu.on('click', function(e) {
-    drawer.toggleClass('open');
-    e.stopPropagation();
-});
+// menu.on('click', function(e) {
+//     drawer.toggleClass('open');
+//     e.stopPropagation();
+// });
 
-main.on('click', function() {
-    drawer.removeClass('open');
-});
+// main.on('click', function() {
+//     drawer.removeClass('open');
+// });
 
+// Color overlay on project on hover
 
-function headerAnimate() {
-    // Shrink navigation & animate header opacity, title & subtitle on scroll
-    var distanceY = $(window).scrollTop(),
-    winWidth = $(window).width(),
-    docWidth = $(document).width(),
-    shrinkOn = 300;
+$('.project').hover(
+    function(e) {
+        var projInner = $(this).find('.project-inner');
+        // var randomNum = Math.floor((Math.random() * 3) + 1);
+        var color = 'rgba(213, 231, 96, 0.6)';
 
-    for (i = 0; i < headerElements.length; i++) {
-        if (distanceY > shrinkOn) {
-            headerElements[i].addClass('smaller');
-        } else {
-            if (distanceY <= shrinkOn) {
-                headerElements[i].removeClass('smaller');
-            }
+        // switch (randomNum) {
+        //     case 1:
+        //         color = 'rgba(105, 105, 105, 0.6)'; // Dark Gray
+        //         break;
+        //     case 2:
+        //         color = 'rgba(213, 231, 96, 0.6)'; // Lime
+        //         break;
+        //     case 3:
+        //         color = 'rgba(34, 33, 122, 0.6)'; // Dark Royal Blue
+        //         break;
+        //     default:
+        //         color = 'rgba(213, 231, 96, 0.6)';
+        // };
+
+        // if ( $(this).hasClass('active') == false ) {
+            projInner.css('background-color', color);
+            projInner.toggleClass('hover');
+        // }
+    }
+);
+
+$('.nav-item').click(
+    function(e) {
+        var pageId = $(this).attr('href');
+
+        $('body').animate({
+            scrollLeft: $(pageId).offset().left
+        }, 500);
+
+        // var page = pageId.slice(1);
+        // var pageClass = '.' + page;
+        // var pageNav = $(pageId).find('.nav ' + pageClass + ' a');
+
+        // pageNav.addClass('active');
+    }
+);
+
+$('.project').click(
+    function(e) {
+        var target = $(this);
+        var scrollPosition = $('.slider-work').scrollTop();
+        projAnimate(target, scrollPosition);
+    }
+);
+
+function projAnimate(target, scrollPosition) {
+    $('.slider-work').animate({
+        scrollTop: 0
+    }, 0);
+
+    var workGrid = $('.slider-work .grid-container');
+    var featuredContainer = $('.featured-container');
+    var summaryContainer = $('.summary-container');
+    var projData = target.attr('data-project');
+    var featuredProj = featuredContainer.find("[data-project='" + projData + "']");
+    var featuredSummary = summaryContainer.find("[data-project='" + projData + "']");
+
+    workGrid.addClass('active');
+
+    featuredContainer.find('.selected').removeClass('selected');
+    featuredProj.addClass('selected');
+
+    summaryContainer.find('.selected').removeClass('selected');
+    featuredSummary.addClass('selected');
+
+    $('.back').click(
+        function(e) {
+            $('.slider-work').animate({
+                scrollTop: scrollPosition
+            }, 0);
+
+            workGrid.removeClass('active');
+
+            featuredContainer.find('.selected').removeClass('selected');
+            featuredContainer.find('.project-1').addClass('selected');
+
+            summaryContainer.find('.selected').removeClass('selected');
         }
-    }
-
-    // Animate header text
-    var title = $('#title'),
-        subtitle = $('#subtitle'),
-        mainImg = $('#main-img');
-
-    if (distanceY > 0) {
-        title.css( { opacity : (1 / distanceY) * 200 - 0.4 } );
-        subtitle.css( { opacity : (1 / distanceY) * 200 - 0.4 } );
-        mainImg.css( { opacity : (1 / distanceY) * 200 - 0.25 } );
-    }
-
-    if (winWidth > 767) {
-        title.css( { marginRight : distanceY } );
-        title.css( { marginBottom : (distanceY / 4) } );
-        subtitle.css( { marginLeft : distanceY } );
-        subtitle.css( { marginTop : (distanceY / 4) } );
-    }
-
-    if (title.css('opacity') == 0) {
-        title.css( { 'display' : 'none' } );
-        subtitle.css( { 'display' : 'none'  } );
-    } else {
-        title.css( { 'display' : 'block' } );
-        subtitle.css( { 'display' : 'block'  } );
-    }
-}
+    );
+};
 
 // Fade in images when visible
-function fadeIn(elem) {
-    for (i = 0; i < elementsToFade.length; i++) {
-        if (($(window).scrollTop() + ($(window).height())) > $(elementsToFade[i]).offset().top + 200) {
-            $(elementsToFade[i]).fadeTo(700, 1);
-        }
-    }
-}
+// function fadeIn(elem) {
+//     for (i = 0; i < elementsToFade.length; i++) {
+//         if (($(window).scrollTop() + ($(window).height())) > $(elementsToFade[i]).offset().top + 200) {
+//             $(elementsToFade[i]).fadeTo(700, 1);
+//         }
+//     }
+// }
 
-$(window).on('scroll', function($){
-   headerAnimate();
-   fadeIn(elementsToFade);
-});
+// $(window).on('scroll', function($){
+//     fadeIn(elementsToFade);
+// });
